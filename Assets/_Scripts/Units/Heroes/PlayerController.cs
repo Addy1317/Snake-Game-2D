4b1c12591b5afc826770f448af0a057cdcda33c0
Aditya Singh <s.addy1317@gmail.com>
2023-07-29 00:14:21 +0530

    Game Update  : - Player (Snake) Movement

--- Assets/_Scripts/Units/Heroes/PlayerController.cs
+++ Assets/_Scripts/Units/Heroes/PlayerController.cs
@@ -7,6 +7,39 @@ namespace SnakeGame
 {
     public class PlayerController : MonoBehaviour
     {
-        
+
+        private Vector2 _direction = Vector2.right;
+
+        private void Update()
+        {
+            SnakeMovement();  
+        }
+
+        private void SnakeMovement()
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                _direction = Vector2.up;
+                Debug.Log("Up");
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                _direction = Vector2.down;   
+            }
+            else if(Input.GetKeyDown(KeyCode.A))
+            {
+                _direction = Vector2.left;
+            }
+            else if(Input.GetKeyDown(KeyCode.D))
+            {
+                _direction = Vector2.right;
+            }
+
+        }
+
+        private void FixedUpdate()
+        {
+            this.transform.position = new Vector3(Mathf.Round(this.transform.position.x) + _direction.x, Mathf.Round(this.transform.position.y) + _direction.y, 0.0f);
+        }
     }
 }
